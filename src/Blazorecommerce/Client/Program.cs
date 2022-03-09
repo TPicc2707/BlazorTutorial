f8b4842cb763fcf83bf483ff5579bb833c82e71e
Tony Piccirilli <anpicc01@louisville.edu>
2022-03-09 09:53:12 -0500

    Create services classes to work with Dependency Injection and have a separation of concern

--- src/Blazorecommerce/Client/Program.cs
+++ src/Blazorecommerce/Client/Program.cs
@@ -1,3 +1,6 @@
+global using Blazorecommerce.Shared;
+global using System.Net.Http.Json;
+global using Blazorecommerce.Client.Services.ProductService;
 using Blazorecommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
---
+++ src/Blazorecommerce/Server/Services/ProductService/IProductService.cs
@@ -0,0 +1,7 @@
+﻿namespace Blazorecommerce.Server.Services.ProductService
+{
+    public interface IProductService
+    {
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
+    }
+}
