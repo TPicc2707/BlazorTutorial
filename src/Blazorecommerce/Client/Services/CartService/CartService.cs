104f71ab06be8cb118dfa9d09aec94f1e08b51d2
dev.one <dev.one@gitlab.com>
2022-05-10 15:04:18 -0400

    Adding Products by user and getting orders per user and seeing details of order

--- src/Blazorecommerce/Client/Services/CartService/CartService.cs
+++ src/Blazorecommerce/Client/Services/CartService/CartService.cs
@@ -6,21 +6,21 @@ namespace Blazorecommerce.Client.Services.CartService
     {
         private readonly ILocalStorageService _localStorage;
         private readonly HttpClient _http;
-        private readonly AuthenticationStateProvider _authStateProvider;
+        private readonly IAuthService _authService;
 
         public CartService(ILocalStorageService localStorage, HttpClient http,
-            AuthenticationStateProvider authStateProvider)
+            IAuthService authService)
         {
             _localStorage = localStorage;
             _http = http;
-            _authStateProvider = authStateProvider;
+            _authService = authService;
         }
 
         public event Action OnChange;
 
         public async Task AddToCart(CartItem cartItem)
         {
-            if (await IsUserAuthenticated())
+            if (await _authService.IsUserAuthenticated())
             {
                 await _http.PostAsJsonAsync("api/cart/add", cartItem);
             }
@@ -50,7 +50,7 @@ namespace Blazorecommerce.Client.Services.CartService
 
         public async Task GetCartItemsCount()
         {
-            if(await IsUserAuthenticated())
+            if(await _authService.IsUserAuthenticated())
             {
                 var result = await _http.GetFromJsonAsync<ServiceResponse<int>>("api/cart/count");
                 var count = result.Data;
@@ -68,7 +68,7 @@ namespace Blazorecommerce.Client.Services.CartService
 
         public async Task<List<CartProductResponse>> GetCartProducts()
         {
-            if(await IsUserAuthenticated())
+            if(await _authService.IsUserAuthenticated())
             {
                 var response = await _http.GetFromJsonAsync<ServiceResponse<List<CartProductResponse>>>("api/cart");
                 return response.Data;
@@ -87,7 +87,7 @@ namespace Blazorecommerce.Client.Services.CartService
 
         public async Task RemoveProductFromCart(int productId, int productTypeId)
         {
-            if(await IsUserAuthenticated())
+            if(await _authService.IsUserAuthenticated())
             {
                 await _http.DeleteAsync($"api/cart/{productId}/{productTypeId}");
             }
@@ -130,7 +130,7 @@ namespace Blazorecommerce.Client.Services.CartService
 
         public async Task UpdateQuantity(CartProductResponse product)
         {
-            if(await IsUserAuthenticated())
+            if(await _authService.IsUserAuthenticated())
             {
                 var request = new CartItem
                 {
@@ -159,11 +159,6 @@ namespace Blazorecommerce.Client.Services.CartService
                 }
             }
 
-        }
-
-        private async Task<bool> IsUserAuthenticated()
-        {
-            return (await _authStateProvider.GetAuthenticationStateAsync()).User.Identity.IsAuthenticated;
         }
 
     }
---
+++ src/Blazorecommerce/Client/Services/OrderService/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazorecommerce.Client.Services.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _http;
+        private readonly IAuthService _authService;
+        private readonly NavigationManager _navigationManager;
+
+        public OrderService(HttpClient http,
+            IAuthService authService,
+            NavigationManager navigationManager)
+        {
+            _http = http;
+            _authService = authService;
+            _navigationManager = navigationManager;
+        }
+
+        public async Task<OrderDetailsResponse> GetOrderDetails(int orderId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<OrderDetailsResponse>>($"api/order/{orderId}");
+            return result.Data;
+        }
+
+        public async Task<List<OrderOverviewResponse>> GetOrders()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<OrderOverviewResponse>>>("api/order");
+            return result.Data;
+        }
+
+        public async Task PlaceOrder()
+        {
+            if(await _authService.IsUserAuthenticated())
+            {
+                await _http.PostAsync("api/order", null);
+            }
+            else
+            {
+                _navigationManager.NavigateTo("login");
+            }
+        }
+    }
+}
