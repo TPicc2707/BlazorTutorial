de042a51b98fd69de317bdedce9d10de3872bc85
dev.one <dev.one@gitlab.com>
2022-05-18 11:54:51 -0400

    Adding Payment to orders to fulfill each order

--- src/Blazorecommerce/Server/Program.cs
+++ src/Blazorecommerce/Server/Program.cs
@@ -6,6 +6,7 @@ global using Blazorecommerce.Server.Services.CategoryService;
 global using Blazorecommerce.Server.Services.CartService;
 global using Blazorecommerce.Server.Services.AuthService;
 global using Blazorecommerce.Server.Services.OrderService;
+global using Blazorecommerce.Server.Services.PaymentService;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -29,6 +30,7 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
