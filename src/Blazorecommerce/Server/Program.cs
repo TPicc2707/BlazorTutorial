24c2080e7c63f76b38372b0b76d10a48228d382d
dev.one <dev.one@gitlab.com>
2022-06-20 10:16:23 -0400

    Create Admin Dashboard

--- src/Blazorecommerce/Server/Program.cs
+++ src/Blazorecommerce/Server/Program.cs
@@ -8,6 +8,7 @@ global using Blazorecommerce.Server.Services.AuthService;
 global using Blazorecommerce.Server.Services.OrderService;
 global using Blazorecommerce.Server.Services.PaymentService;
 global using Blazorecommerce.Server.Services.AddressService;
+global using Blazorecommerce.Server.Services.ProductTypeService;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -33,6 +34,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
